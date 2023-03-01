172909509e1b92c481ec78c14d6e8d07ba698b77
kevin344k <105673385+kevin344k@users.noreply.github.com>
2023-03-01 23:16:54 +0000

    .

--- public/script.js
+++ public/script.js
@@ -1,5 +1,31 @@
 const socket=io()
 
+
+
+    const butRun=document.getElementById("butRun")
+
+    butRun.addEventListener("click",()=>{
+        setInterval(()=>{
+        if (butRun.style.backgroundColor="#198754") {
+        butRun.style.backgroundColor="white"
+        butRun.style.color="#198754"
+        console.log("VERDE")
+        }
+         setInterval(() => {
+            if (butRun.style.backgroundColor="white") {
+            butRun.style.backgroundColor="#198754" 
+            butRun.style.color="white"
+            console.log("bl")
+        }
+         }, 3000);
+    }, 2000);
+        
+    })
+
+
+   
+
+
 socket.on('server:hola',()=>{
-  alert('hola estoy con vida')
+  console.log('hola estoy con vida')
 })
